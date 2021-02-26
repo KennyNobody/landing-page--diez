40c80ad17b85c780e147399872779e9863ea6e7c
Kenny <nobodykenny@gmail.com>
2021-02-26 12:00:13 +0300

    'work'

--- src/blocks/modules/menu-link/menu-link.js
+++ src/blocks/modules/menu-link/menu-link.js
@@ -1,13 +1,18 @@
-modules.define('menu-link', ['i-bem-dom'], function(provide, bemDom) {
+document.addEventListener('DOMContentLoaded', () => {
+	const button = document.querySelector('.menu-link');
+	const overlay = document.querySelector('.menu__overlay');
+	const body = document.querySelector('.body');
 
-provide(bemDom.declBlock(this.name, {
-    onSetMod: {
-        js: {
-            inited: function() {
-                
-            }
-        }
-    }
-}));
+	if (button) {
+		button.addEventListener('click', function(e) {
+			e.preventDefault();
+			body.classList.toggle('body--menu-opened');
+		});
+	}
 
+	if (overlay) {
+		overlay.addEventListener('click', function() {
+			body.classList.remove('body--menu-opened');
+		});
+	}
 });
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,3 +1,5 @@
 import "%modules%/header/header";
 import "%modules%/footer/footer";
+import "%modules%/menu-link/menu-link";
+import "%modules%/menu/menu";
 import "./agreement";
