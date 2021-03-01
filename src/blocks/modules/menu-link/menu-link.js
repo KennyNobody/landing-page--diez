6c9deb12643ea7945c440996eed4c7b1a625ed97
Kenny <nobodykenny@gmail.com>
2021-03-01 08:03:39 +0300

    'work'

--- src/blocks/modules/menu-link/menu-link.js
+++ src/blocks/modules/menu-link/menu-link.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	const button = document.querySelector('.menu-link');
 	const overlay = document.querySelector('.menu__overlay');
 	const body = document.querySelector('.body');
+	const links = document.querySelectorAll('.menu a');
 
 	if (button) {
 		button.addEventListener('click', function(e) {
@@ -14,5 +15,13 @@ document.addEventListener('DOMContentLoaded', () => {
 		overlay.addEventListener('click', function() {
 			body.classList.remove('body--menu-opened');
 		});
+	}
+
+	if (links) {
+		for (let i = 0; i < links.length; i++) {
+			links[i].addEventListener('click', function() {
+				body.classList.remove('body--menu-opened');
+			});
+		}
 	}
 });
